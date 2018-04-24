4d7b28948762e6a37e7ca426298d098c3e6970f0
Lee Fogg <leefogg@users.noreply.github.com>
2018-04-24 23:01:28 +0100

    TODO

--- src/engine/graphics/models/GLArrayType.java
+++ src/engine/graphics/models/GLArrayType.java
@@ -2,6 +2,7 @@ package engine.graphics.models;
 
 import org.lwjgl.opengl.GL15;
 
+//TODO: Add all types from https://www.khronos.org/registry/OpenGL-Refpages/gl4/html/glBufferData.xhtml
 public enum GLArrayType {
 	Array(GL15.GL_ARRAY_BUFFER),
 	Element(GL15.GL_ELEMENT_ARRAY_BUFFER);
