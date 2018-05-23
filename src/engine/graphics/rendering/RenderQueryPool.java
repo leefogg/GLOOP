f58e88bdac0e27d8d2bfe34e1c263bd781c1397d
Lee Fogg <leefogg@users.noreply.github.com>
2018-05-23 23:13:28 +0100

    Optimization: Next GPUQuery in list is probably last to finish

--- src/engine/graphics/rendering/RenderQueryPool.java
+++ src/engine/graphics/rendering/RenderQueryPool.java
@@ -4,27 +4,47 @@ import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL15;
 
 import java.nio.IntBuffer;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public class RenderQueryPool {
-	private static final HashSet<GPUQuery> Pool = new HashSet<>(10);
+	private static final ArrayList<GPUQuery> Pool = new ArrayList<>(10);
+	private static int NextQuery = 0;
 
 	static {
-		for (int i=0; i<Pool.size(); i++)
+		for (int i=0; i<10; i++)
 			Pool.add(new GPUQuery(GPUQuery.Type.AnySamplesPassed));
 	}
 
 	public static GPUQuery getQuery(GPUQuery.Type type) {
-		//TODO: Optimization: Seek from last query index around the array
-		for (GPUQuery query : Pool)
-			if (query.getType() == type)
-				if (!query.isInUse())
-					return query;
-		System.out.println("Generating new query object " + (Pool.size()+1));
+		int start = NextQuery;
+		NextQuery++;
+		NextQuery = NextQuery % Pool.size();
+
+		int i = start;
+		for (; i<Pool.size(); i++) {
+			GPUQuery query = isPoolIndexAvailable(i, type);
+			if (query != null)
+				return query;
+		}
+		for (i = 0; i<start; i++) {
+			GPUQuery query = isPoolIndexAvailable(i, type);
+			if (query != null)
+				return query;
+		}
+
+
 		return new GPUQuery(type);
 	}
 
-	static void register(GPUQuery query) { Pool.add(query); }
+	private static GPUQuery isPoolIndexAvailable(int i, GPUQuery.Type type) {
+		GPUQuery query = Pool.get(i);
+		if (query.getType() == type)
+			if (!query.isInUse())
+				return query;
+		return null;
+	}
+
+	static void register(GPUQuery query) { Pool.add(query); System.out.println("Generating new query object " + (Pool.size()+1));}
 
 	static void unregister(GPUQuery query) { Pool.remove(query); }
 
