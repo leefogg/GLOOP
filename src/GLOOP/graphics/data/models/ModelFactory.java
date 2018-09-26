e2dc03f6b0e6f2abcdec1fdb57c064683f522d35
Lee Fogg <leefogg@users.noreply.github.com>
2018-09-26 20:41:53 +0100

    Some reorganizing of ModelFactory

--- src/GLOOP/graphics/data/models/ModelFactory.java
+++ src/GLOOP/graphics/data/models/ModelFactory.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class ModelFactory {
-	static class GeometryCacheModel {
+	private static class GeometryCacheModel {
 		public VertexArray vao;
 		public AABB boundingBox;
 
@@ -28,36 +28,53 @@ public class ModelFactory {
 	}
 
 	public static final Model3D getModel(String filepath, Material material) throws IOException, UnsupportedException {
-		Model3D cachedmodel = getModelFromCache(filepath, material);
+		Model3D cachedmodel = createModelFromCache(filepath, material);
 		if (cachedmodel != null)
 			return cachedmodel;
 
-		// None found
+		// Wasn't in cache, so load it
 		System.out.println("First load. Returning new.");
-		// Load
+
+		getModelVAO(filepath);
+		// And now it'll be in cache so return it
+		return createModelFromCache(filepath, material);
+	}
+
+	private static Model3D createModelFromCache(String filepath, Material material) {
+		GeometryCacheModel cachedmodel = getModelFromCache(filepath);
+		if (cachedmodel != null)
+			return createModel(cachedmodel, material);
+		return null;
+	}
+
+	private static final VertexArray getModelVAO(String filepath) throws IOException, UnsupportedException {
 		Geometry geo = loadGeometry(filepath);
 		VertexArray newvao = new VertexArray(filepath); // Saved automatically
 		newvao.storeMesh(geo);
 
 		registerModel(newvao, geo.getBoundingBox());
 
-		return getModelFromCache(filepath, material);
+		return newvao;
+	}
+
+	private static GeometryCacheModel getModelFromCache(String path) {
+		// This method will only be called when we just added a new model
+		// so most likely will be at the end, so search in last to first order
+		for (int i=Cache.size()-1; i>=0; i--) {
+			GeometryCacheModel model = Cache.get(i);
+			if (model.vao.getName().contentEquals(path))
+				return model;
+		}
+
+		return null;
 	}
 
 	private static final void registerModel(VertexArray vao, AABB boundingbox) {
 		Cache.add(new GeometryCacheModel(vao, boundingbox));
 	}
 
-	private static Model3D getModelFromCache(String path, Material material) {
-		// This method will only be called when we just added a new model
-		// so most likely will be at the end, so search in last to first order
-		for (int i=Cache.size()-1; i>=0; i--) {
-			GeometryCacheModel model = Cache.get(i);
-			if (model.vao.getName().contentEquals(path))
-				return new Model3D(model.vao, material, model.boundingBox);
-		}
-
-		return null;
+	private static final Model3D createModel(GeometryCacheModel model, Material material) {
+		return new Model3D(model.vao, material, model.boundingBox);
 	}
 
 	static Geometry loadGeometry(String filepath) throws UnsupportedException, IOException {
